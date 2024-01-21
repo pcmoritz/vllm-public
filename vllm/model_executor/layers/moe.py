a263fa6efecc3ebff544282be4b1ec620409684d
Philipp Moritz <pcmoritz@gmail.com>
2024-01-21 12:23:35 -0800

    update

--- vllm/model_executor/layers/moe.py
+++ vllm/model_executor/layers/moe.py
@@ -92,9 +92,9 @@ class MoE(nn.Module):
                         selected_experts: torch.Tensor,
                         routing_weights: torch.Tensor) -> torch.Tensor:
         return fused_moe(hidden_states,
-                         self.w1s.view(-1, *self.w1s.shape[2:]),
-                         self.w2s.view(-1, *self.w2s.shape[2:]),
-                         self.w3s.view(-1, *self.w3s.shape[2:]),
+                         self.w1s.transpose(-1, -2),
+                         self.w2s.transpose(-1, -2),
+                         self.w3s.transpose(-1, -2),
                          routing_weights,
                          selected_experts,
                          inplace=True)

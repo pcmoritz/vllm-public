76e49c52d5577ccc3998555ebd618963113ba22a
Philipp Moritz <pcmoritz@gmail.com>
2024-02-02 19:54:41 -0800

    update

--- vllm/model_executor/layers/fused_moe.py
+++ vllm/model_executor/layers/fused_moe.py
@@ -79,65 +79,85 @@ def fused_moe_kernel(
     offs_token = tl.load(sorted_token_ids_ptr + offs_token_id)
     token_mask = offs_token < num_valid_tokens
 
-    offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
     offs_k = tl.arange(0, BLOCK_SIZE_K)
     a_ptrs = a_ptr + (offs_token[:, None] // top_k * stride_am +
                       offs_k[None, :] * stride_ak)
 
     off_experts = tl.load(expert_ids_ptr + pid_m)
-    b_ptrs = b_ptr + off_experts * stride_be + (offs_k[:, None] * stride_bk +
-                                                offs_bn[None, :] * stride_bn)
 
-    # -----------------------------------------------------------
-    # Iterate to compute a block of the C matrix.
-    # We accumulate into a `[BLOCK_SIZE_M, BLOCK_SIZE_N]` block
-    # of fp32 values for higher accuracy.
-    # `accumulator` will be converted back to fp16 after the loop.
-    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    if not FUSE_SILU:
+        offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+        b_ptrs = b_ptr + off_experts * stride_be + (offs_k[:, None] * stride_bk +
+                                                    offs_bn[None, :] * stride_bn)
+        accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
 
-    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
-        # Load the next block of A and B, generate a mask by checking the K dimension.
-        a = tl.load(a_ptrs,
-                    mask=token_mask[:, None] &
-                    (offs_k[None, :] < K - k * BLOCK_SIZE_K),
-                    other=0.0)
-        b = tl.load(b_ptrs,
-                    mask=offs_k[:, None] < K - k * BLOCK_SIZE_K,
-                    other=0.0)
-        # We accumulate along the K dimension.
-        accumulator += tl.dot(a, b)
-        # Advance the ptrs to the next K block.
-        a_ptrs += BLOCK_SIZE_K * stride_ak
-        b_ptrs += BLOCK_SIZE_K * stride_bk
+        for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+            # Load the next block of A and B, generate a mask by checking the K dimension.
+            a = tl.load(a_ptrs,
+                        mask=token_mask[:, None] &
+                        (offs_k[None, :] < K - k * BLOCK_SIZE_K),
+                        other=0.0)
+            b = tl.load(b_ptrs,
+                        mask=offs_k[:, None] < K - k * BLOCK_SIZE_K,
+                        other=0.0)
+            # We accumulate along the K dimension.
+            accumulator += tl.dot(a, b)
+            # Advance the ptrs to the next K block.
+            a_ptrs += BLOCK_SIZE_K * stride_ak
+            b_ptrs += BLOCK_SIZE_K * stride_bk
 
-    if MUL_ROUTED_WEIGHT:
         moe_weight = tl.load(topk_weights_ptr + offs_token,
                              mask=token_mask,
                              other=0)
         accumulator = accumulator * moe_weight[:, None]
 
-    if not FUSE_SILU:
-        result = accumulator.to(compute_type)
-    else:
-        idx1 = tl.arange(0, BLOCK_SIZE_N//2)
-        idx2 = tl.arange(BLOCK_SIZE_N//2, BLOCK_SIZE_N)
-        x = accumulator[:,idx1]
-        y = accumulator[:,idx2]
-        result = x * tl.sigmoid(y).to(compute_type)
+        accumulator = accumulator.to(compute_type)
 
-    accumulator = accumulator.to(compute_type)
-
-    # -----------------------------------------------------------
-    # Write back the block of the output
-    if not FUSE_SILU:
         offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+
+        c_ptrs = c_ptr + stride_cm * offs_token[:, None] + stride_cn * offs_cn[
+            None, :]
+        c_mask = token_mask[:, None] & (offs_cn[None, :] < N)
+        tl.store(c_ptrs, result, mask=c_mask)
+
     else:
+        offs_bn1 = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N//2)) % N
+        offs_bn2 = (pid_n * BLOCK_SIZE_N + tl.arange(BLOCK_SIZE_N//2, BLOCK_SIZE_N)) % N
+        b_ptrs1 = b_ptr + off_experts * stride_be + (offs_k[:, None] * stride_bk +
+                                                     offs_bn1[None, :] * stride_bn)
+        b_ptrs2 = b_ptr + off_experts * stride_be + (offs_k[:, None] * stride_bk +
+                                                     offs_bn2[None, :] * stride_bn)
+        acc1 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N//2), dtype=tl.float32)
+        acc2 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N//2), dtype=tl.float32)
+
+        for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+            # Load the next block of A and B, generate a mask by checking the K dimension.
+            a = tl.load(a_ptrs,
+                        mask=token_mask[:, None] &
+                        (offs_k[None, :] < K - k * BLOCK_SIZE_K),
+                        other=0.0)
+            b1 = tl.load(b_ptrs1,
+                        mask=offs_k[:, None] < K - k * BLOCK_SIZE_K,
+                        other=0.0)
+            b2 = tl.load(b_ptrs2,
+                        mask=offs_k[:, None] < K - k * BLOCK_SIZE_K,
+                        other=0.0)
+            # We accumulate along the K dimension.
+            acc1 += tl.dot(a, b1)
+            acc2 += tl.dot(a, b2)
+            # Advance the ptrs to the next K block.
+            a_ptrs += BLOCK_SIZE_K * stride_ak
+            b_ptrs1 += BLOCK_SIZE_K * stride_bk
+            b_ptrs2 += BLOCK_SIZE_K * stride_bk
+
+        result = acc1 * tl.sigmoid(acc2).to(compute_type)
+
         offs_cn = pid_n * BLOCK_SIZE_N//2 + tl.arange(0, BLOCK_SIZE_N//2)
 
-    c_ptrs = c_ptr + stride_cm * offs_token[:, None] + stride_cn * offs_cn[
-            None, :]
-    c_mask = token_mask[:, None] & (offs_cn[None, :] < N)
-    tl.store(c_ptrs, result, mask=c_mask)
+        c_ptrs = c_ptr + stride_cm * offs_token[:, None] + stride_cn * offs_cn[
+                None, :]
+        c_mask = token_mask[:, None] & (offs_cn[None, :] < N)
+        tl.store(c_ptrs, result, mask=c_mask)
 
 
 def moe_align_block_size(

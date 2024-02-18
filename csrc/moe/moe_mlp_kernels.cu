a3c632f19c19889c8d9e02070c9ee954ad113ee9
Philipp Moritz <pcmoritz@gmail.com>
2024-02-17 18:09:36 -0800

    update

--- csrc/moe/moe_mlp_kernels.cu
+++ csrc/moe/moe_mlp_kernels.cu
@@ -7,135 +7,98 @@
 #include <c10/util/BFloat16.h>
 #include <c10/cuda/CUDAStream.h>
 
-#include "cutlass/cutlass.h"
-
-#include "cute/tensor.hpp"
-#include "cutlass/tensor_ref.h"
-#include "cutlass/epilogue/collective/default_epilogue.hpp"
-#include "cutlass/epilogue/thread/linear_combination.h"
-#include "cutlass/gemm/dispatch_policy.hpp"
-#include "cutlass/gemm/group_array_problem_shape.hpp"
-#include "cutlass/gemm/collective/collective_builder.hpp"
-#include "cutlass/epilogue/collective/collective_builder.hpp"
-#include "cutlass/gemm/device/gemm_universal_adapter.h"
-#include "cutlass/gemm/kernel/gemm_universal.hpp"
-
-#include "cutlass/util/packed_stride.hpp"
-#include "cutlass/util/device_memory.h"
-
-using namespace cute;
+#include "cutlass/bfloat16.h"
+#include "cutlass/complex.h"
+#include "cutlass/gemm/kernel/gemm_grouped.h"
+#include "cutlass/gemm/kernel/default_gemm_grouped.h"
+#include "cutlass/gemm/device/gemm_grouped.h"
 
 namespace vllm {
 
-using ProblemShape = cutlass::gemm::GroupProblemShape<Shape<int,int,int>>;  // <M,N,K> per group
-using ElementA = cutlass::bfloat16_t;                                       // Element type for A matrix operand
-using ElementB = cutlass::bfloat16_t;                                       // Element type for B matrix operand
-using ElementC = cutlass::bfloat16_t;                                       // Element type for C and D matrix operands
+#define CUDA_CALL(code)					                    \
+  do {                                              \
+    cudaError_t status = code;                      \
+    std::string err = cudaGetErrorString(status);   \
+    TORCH_CHECK(status == cudaSuccess, err);		    \
+  } while (0)
 
-// A matrix configuration
-using         LayoutA     = cutlass::layout::RowMajor;                      // Layout type for A matrix operand
-constexpr int AlignmentA  = 128 / cutlass::sizeof_bits<ElementA>::value;    // Memory access granularity/alignment of A matrix in units of elements (up to 16 bytes)
+using GroupedGemmKernelNN = typename cutlass::gemm::kernel::DefaultGemmGrouped<
+  // Non-transposed A operand.
+  ::cutlass::bfloat16_t,
+  ::cutlass::layout::RowMajor,
+  ::cutlass::ComplexTransform::kNone,
+  8,
+  // Non-transposed B operand.
+  ::cutlass::bfloat16_t,
+  ::cutlass::layout::RowMajor,
+  ::cutlass::ComplexTransform::kNone,
+  8,
+  // C operand.
+  ::cutlass::bfloat16_t,
+  ::cutlass::layout::RowMajor,
+  float,
+  ::cutlass::arch::OpClassTensorOp,
+  ::cutlass::arch::Sm80,
+  ::cutlass::gemm::GemmShape<128, 128, 32>,
+  ::cutlass::gemm::GemmShape<64, 64, 32>,
+  ::cutlass::gemm::GemmShape<16, 8, 16>,
+  ::cutlass::epilogue::thread::LinearCombination<::cutlass::bfloat16_t, 8, float, float>,
+  // NOTE: Threadblock swizzling is currently not supported by CUTLASS's grouped kernels.
+  // This parameter is passed in at present to match the APIs of other kernels. The parameter
+  // is unused within the kernel.
+  ::cutlass::gemm::threadblock::GemmBatchedIdentityThreadblockSwizzle,
+  4>::GemmKernel;
+using GemmGroupedNN = ::cutlass::gemm::device::GemmGrouped<GroupedGemmKernelNN>;
 
-// B matrix configuration
-using         LayoutB     = cutlass::layout::RowMajor;                   // Layout type for B matrix operand
-constexpr int AlignmentB  = 128 / cutlass::sizeof_bits<ElementB>::value;    // Memory access granularity/alignment of B matrix in units of elements (up to 16 bytes)
 
-// C/D matrix configuration
-using         LayoutC     = cutlass::layout::RowMajor;                   // Layout type for C and D matrix operands
-constexpr int AlignmentC  = 128 / cutlass::sizeof_bits<ElementC>::value;    // Memory access granularity/alignment of C matrix in units of elements (up to 16 bytes)
-
-// Core kernel configurations
-using ElementAccumulator  = float;                                          // Element type for internal accumulation
-using ArchTag             = cutlass::arch::Sm90;                            // Tag indicating the minimum SM that supports the intended feature
-using OperatorClass       = cutlass::arch::OpClassTensorOp;                 // Operator class tag
-using TileShape           = Shape<_128,_128,_64>;                           // Threadblock-level tile size
-using ClusterShape        = Shape<_2,_1,_1>;                                // Shape of the threadblocks in a cluster
-using StageCountType = cutlass::gemm::collective::StageCountAuto;           // Stage count maximized based on the tile size
-using KernelSchedule = cutlass::gemm::KernelPtrArrayTmaWarpSpecializedCooperative; // Kernel to launch
-using EpilogueSchedule = cutlass::epilogue::PtrArrayNoSmemWarpSpecialized;                     // Epilogue to launch
-
-using CollectiveEpilogue = typename cutlass::epilogue::collective::CollectiveBuilder<
-    cutlass::arch::Sm90, cutlass::arch::OpClassTensorOp,
-    TileShape, ClusterShape,
-    cutlass::epilogue::collective::EpilogueTileAuto,
-    ElementAccumulator, ElementAccumulator,
-    ElementC, LayoutC *, AlignmentC,
-    ElementC, LayoutC *, AlignmentC,
-    EpilogueSchedule
-  >::CollectiveOp;
-
-using CollectiveMainloop = typename cutlass::gemm::collective::CollectiveBuilder<
-    ArchTag, OperatorClass,
-    ElementA, LayoutA *, AlignmentA,
-    ElementB, LayoutB *, AlignmentB,
-    ElementAccumulator,
-    TileShape, ClusterShape,
-    cutlass::gemm::collective::StageCountAutoCarveout<
-      static_cast<int>(sizeof(typename CollectiveEpilogue::SharedStorage))>,
-    KernelSchedule
-  >::CollectiveOp;
-
-using GemmKernel = cutlass::gemm::kernel::GemmUniversal<
-    ProblemShape,
-    CollectiveMainloop,
-    CollectiveEpilogue
->;
-
-using Gemm = cutlass::gemm::device::GemmUniversalAdapter<GemmKernel>;
-
-std::vector<typename ProblemShape::UnderlyingProblemShape> MakeProblemSizes(torch::Tensor b, torch::Tensor cum_num_tokens_per_expert) {
+std::vector<cutlass::gemm::GemmCoord> MakeProblemSizes(torch::Tensor b, torch::Tensor cum_num_tokens_per_expert) {
   const size_t num_experts = cum_num_tokens_per_expert.size(0);
   const size_t k = b.size(1), n = b.size(2);
-  std::vector<typename ProblemShape::UnderlyingProblemShape> problem_sizes(num_experts);
-  problem_sizes[0] = {cum_num_tokens_per_expert.data_ptr<int64_t>()[0], n, k};
-  for (int i = 1; i < num_experts; ++i) {
-    int64_t batch_size = cum_num_tokens_per_expert.data_ptr<int64_t>()[i] - cum_num_tokens_per_expert.data_ptr<int64_t>()[i-1];
-    problem_sizes[i] = {batch_size, n, k};
+  std::vector<cutlass::gemm::GemmCoord> problem_sizes(num_experts);
+  for (int i = 0; i < num_experts; ++i) {
+    int64_t batch_size = cum_num_tokens_per_expert.data_ptr<int64_t>()[i] - (i > 0 ? cum_num_tokens_per_expert.data_ptr<int64_t>()[i-1] : 0);
+    problem_sizes[i] = cutlass::gemm::GemmCoord(batch_sizes.data_ptr<int64_t>()[i], n, k);
   }
   return problem_sizes;
 }
 
-template <typename Gemm>
-struct ProblemData {
-  std::vector<typename ProblemShape::UnderlyingProblemShape> problem_sizes_host;
-  cutlass::DeviceAllocation<typename ProblemShape::UnderlyingProblemShape> problem_sizes;
-  cutlass::DeviceAllocation<typename Gemm::ElementA *> ptr_A;
-  cutlass::DeviceAllocation<typename Gemm::ElementB *> ptr_B;
-  cutlass::DeviceAllocation<typename Gemm::ElementC *> ptr_C;
-  cutlass::DeviceAllocation<typename Gemm::GemmKernel::UnderlyingStrideA> stride_A;
-  cutlass::DeviceAllocation<typename Gemm::GemmKernel::UnderlyingStrideB> stride_B;
-  cutlass::DeviceAllocation<typename Gemm::GemmKernel::UnderlyingStrideC> stride_C;
-};
-
 template <typename T>
-void CopyDataToDevice(const std::vector<T> &src, cutlass::DeviceAllocation<T> &target) {
-  target.reset(src.size());
-  target.copy_from_host(src.data());
+torch::Tensor CopyToDevice(const std::vector<T> &x, const torch::Device &device) {
+  size_t bytes = x.size() * sizeof(T);
+  auto options = torch::TensorOptions().dtype(torch::kInt8).device(device);
+  torch::Tensor out = torch::empty(bytes, options);
+
+  CUDA_CALL(cudaMemcpyAsync(out.data_ptr(),
+			    x.data(), bytes,
+			    cudaMemcpyHostToDevice,
+			    c10::cuda::getCurrentCUDAStream()));
+  return out;
 }
 
 template <typename Gemm>
-typename Gemm::Arguments MakeArguments(ProblemData<Gemm>& problem_data,
-               torch::Tensor a,
+typename Gemm::Arguments MakeArguments(torch::Tensor a,
 				       torch::Tensor b,
 				       torch::Tensor c,
-				       torch::Tensor cum_num_tokens_per_expert) {
-  problem_data.problem_sizes_host = MakeProblemSizes(b, cum_num_tokens_per_expert);
+				       torch::Tensor batch_sizes) {
+  auto problem_sizes_host = MakeProblemSizes(b, batch_sizes);
 
   // Calculate the number of threadblocks to use and validate the result.
-  int64_t num_experts = problem_data.problem_sizes_host.size();
+  int64_t num_experts = problem_sizes_host.size();
+
+  // NOTE: This is borrowed from FasterTransformer.
+  int threadblock_count = Gemm::sufficient(problem_sizes_host.data(), num_experts);
+  if (!threadblock_count) {
+    TORCH_CHECK(false, "Grouped GEMM execution not possible with HW");
+  }
 
   // Create the host arrays of leading dimension data and pointer data.
-  using StrideA = typename Gemm::GemmKernel::UnderlyingStrideA;
-  using StrideB = typename Gemm::GemmKernel::UnderlyingStrideB;
-  using StrideC = typename Gemm::GemmKernel::UnderlyingStrideC;
-  using StrideD = typename Gemm::GemmKernel::UnderlyingStrideD;
+  using LayoutA = typename Gemm::LayoutA;
+  using LayoutB = typename Gemm::LayoutB;
+  using LayoutC = typename Gemm::LayoutC;
 
-  std::vector<int64_t>  offsets_a(num_experts);
-  std::vector<int64_t> offsets_b(num_experts);
-  std::vector<int64_t> offsets_c(num_experts);
-  std::vector<StrideA> stride_a_host;
-  std::vector<StrideB> stride_b_host;
-  std::vector<StrideC> stride_c_host;
+  std::vector<int64_t> lda_host(num_experts), offsets_a(num_experts);
+  std::vector<int64_t> ldb_host(num_experts), offsets_b(num_experts);
+  std::vector<int64_t> ldc_host(num_experts), offsets_c(num_experts);
   int64_t elements_a = 0, elements_b = 0, elements_c = 0;
 
   using ElementA = typename Gemm::ElementA;
@@ -146,14 +109,10 @@ typename Gemm::Arguments MakeArguments(ProblemData<Gemm>& problem_data,
   std::vector<ElementC *> ptr_c_host(num_experts);
 
   for (int i = 0; i < num_experts; ++i) {
-    auto problem = problem_data.problem_sizes_host[i];
-    auto M = get<0>(problem);
-    auto N = get<1>(problem);
-    auto K = get<2>(problem);
-
-    stride_a_host.push_back(cutlass::make_cute_packed_stride(StrideA{}, cute::make_shape(M, K, Int<1>{})));
-    stride_b_host.push_back(cutlass::make_cute_packed_stride(StrideB{}, cute::make_shape(N, K, Int<1>{})));
-    stride_c_host.push_back(cutlass::make_cute_packed_stride(StrideC{}, cute::make_shape(M, N, Int<1>{})));
+    auto problem = problem_sizes_host[i];
+    lda_host[i] = LayoutA::packed({problem.m(), problem.k()}).stride(0);
+    ldb_host[i] = LayoutB::packed({problem.k(), problem.n()}).stride(0);
+    ldc_host[i] = LayoutC::packed({problem.m(), problem.n()}).stride(0);
 
     offsets_a[i] = elements_a;
     offsets_b[i] = elements_b;
@@ -163,68 +122,61 @@ typename Gemm::Arguments MakeArguments(ProblemData<Gemm>& problem_data,
     ptr_b_host[i] = (ElementB*)b.data_ptr() + offsets_b[i];
     ptr_c_host[i] = (ElementC*)c.data_ptr() + offsets_c[i];
 
-    elements_a += M * K;
-    elements_b += K * N;
-    elements_c += M * N;
+    elements_a += problem.m() * problem.k();
+    elements_b += problem.k() * problem.n();
+    elements_c += problem.m() * problem.n();
   }
 
   // Copy the problem sizes, pointers and leading dimension data to the device.
-  CopyDataToDevice(problem_data.problem_sizes_host, problem_data.problem_sizes);
+  torch::Tensor lda = CopyToDevice(lda_host, a.device());
+  torch::Tensor ldb = CopyToDevice(ldb_host, a.device());
+  torch::Tensor ldc = CopyToDevice(ldc_host, a.device());
+  torch::Tensor ptr_a = CopyToDevice(ptr_a_host, a.device());
+  torch::Tensor ptr_b = CopyToDevice(ptr_b_host, a.device());
+  torch::Tensor ptr_c = CopyToDevice(ptr_c_host, a.device());
+  torch::Tensor problem_sizes = CopyToDevice(problem_sizes_host, a.device());
 
-  CopyDataToDevice(ptr_a_host, problem_data.ptr_A);
-  CopyDataToDevice(ptr_b_host, problem_data.ptr_B);
-  CopyDataToDevice(ptr_c_host, problem_data.ptr_C);
-
-  CopyDataToDevice(stride_a_host, problem_data.stride_A);
-  CopyDataToDevice(stride_b_host, problem_data.stride_B);
-  CopyDataToDevice(stride_c_host, problem_data.stride_C);
-
-  cutlass::KernelHardwareInfo hw_info;
-  hw_info.device_id = b.device().index();
-  hw_info.sm_count = cutlass::KernelHardwareInfo::query_device_multiprocessor_count(hw_info.device_id);
-
-  typename Gemm::Arguments arguments{
-    cutlass::gemm::GemmUniversalMode::kGrouped,
-    {static_cast<int>(num_experts), problem_data.problem_sizes.get(), problem_data.problem_sizes_host.data()},
-    {problem_data.ptr_A.get(), problem_data.stride_A.get(),
-     problem_data.ptr_B.get(), problem_data.stride_B.get()},
-    {{/*alpha=*/1.0f, /*beta=*/0.0f},
-     problem_data.ptr_C.get(), problem_data.stride_C.get(),
-     problem_data.ptr_C.get(), problem_data.stride_C.get()},
-    hw_info
-  };
-
+  typename Gemm::EpilogueOutputOp::Params epilogue_op(/*alpha=*/1.0f, /*beta=*/0.0f);
+  typename Gemm::Arguments arguments((cutlass::gemm::GemmCoord*)problem_sizes.data_ptr(),
+  				     (int)num_experts,
+  				     (int)threadblock_count,
+  				     epilogue_op,
+  				     (ElementA**)ptr_a.data_ptr(),
+  				     (ElementB**)ptr_b.data_ptr(),
+  				     (ElementC**)ptr_c.data_ptr(),
+  				     (ElementC**)ptr_c.data_ptr(),
+  				     /*lda=*/(int64_t*)lda.data_ptr(),
+  				     /*ldb=*/(int64_t*)ldb.data_ptr(),
+  				     /*ldc=*/(int64_t*)ldc.data_ptr(),
+  				     /*ldd=*/(int64_t*)ldc.data_ptr(),
+  				     (cutlass::gemm::GemmCoord*)problem_sizes_host.data());
   return arguments;
 }
 
-void CutlassGroupedGemm(
-    torch::Tensor a,
-	torch::Tensor b,
-	torch::Tensor c,
-	torch::Tensor cum_num_tokens_per_expert,
-    cudaStream_t stream) {
+void CutlassGroupedGemm(torch::Tensor a,
+				 torch::Tensor b,
+				 torch::Tensor c,
+				 torch::Tensor cum_num_tokens_per_expert,
+         cudaStream_t stream) {
+  using Gemm = GemmGroupedNN;
   Gemm gemm;
-  ProblemData<Gemm> problem_data;
 
-  auto arguments = MakeArguments<Gemm>(problem_data, a, b, c, cum_num_tokens_per_expert);
+  auto arguments = MakeArguments<Gemm>(a, b, c, cum_num_tokens_per_expert);
   int64_t workspace_size = gemm.get_workspace_size(arguments);
   auto options = torch::TensorOptions().dtype(torch::kInt8).device(a.device());
   torch::Tensor workspace = torch::empty(workspace_size, options);
-
-  // Check if the problem size is supported or not
-  auto status = gemm.can_implement(arguments);
-  TORCH_CHECK(status == cutlass::Status::kSuccess, cutlass::cutlassGetStatusString(status));
 
   // Initialize the kernel.
   if(gemm.initialize(arguments, workspace.data_ptr()) != cutlass::Status::kSuccess) {
     TORCH_CHECK(false, "Failed to initialize CUTLASS Grouped GEMM");
   }
 
-  // Execute the kernel.
+  // Execute the kernel in the current stream.
   if(gemm.run(stream) != cutlass::Status::kSuccess) {
     TORCH_CHECK(false, "Failed to run CUTLASS Grouped GEMM");
   }
 }
+
 
 template <class T, class ActFn>
 __global__ void doGatedActivationKernel(

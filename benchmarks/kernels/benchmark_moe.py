dd0766e32812c402af3b65737afeab8721d294ec
Philipp Moritz <pcmoritz@gmail.com>
2024-02-20 15:58:02 -0800

    update

--- benchmarks/kernels/benchmark_moe.py
+++ benchmarks/kernels/benchmark_moe.py
@@ -14,11 +14,12 @@ def main():
 
 
 def run_grid(method):
-    bs_grid = [1, 2, 4, 8, 16, 32, 64, 128, 1024, 2048, 4096, 8192]
+    bs = 1
+    # bs_grid = [1, 2, 4, 8, 16, 32, 64, 128, 1024, 2048, 4096, 8192]
     d_model = 4096
     num_total_experts = 8
     top_k = 2
-    tp_size_grid = [8]
+    tp_size_grid = [2]
     model_intermediate_size = 14336
     num_layers = 32
     num_calls = 100
@@ -26,9 +27,28 @@ def run_grid(method):
     num_warmup_trials = 1
     num_trials = 1
 
+    configs = []
+    for block_size_n1 in [8, 16]:
+        for block_size_n2 in [8, 16]:
+            for block_size_k1 in [64, 128, 256]:
+                for block_size_k2 in [64, 128, 256]:
+                    configs.append(
+                        {
+                            "BLOCK_SIZE_M": 1,
+                            "BLOCK_SIZE_N1": block_size_n1,
+                            "BLOCK_SIZE_N2": block_size_n2,
+                            "BLOCK_SIZE_K1": block_size_k1,
+                            "BLOCK_SIZE_K2": block_size_k2,
+                            "GROUP_SIZE_M1": 1,
+                            "GROUP_SIZE_M2": 1,
+                        }
+                    )
+
     for tp_size in tp_size_grid:
-        for bs in bs_grid:
+        # for bs in bs_grid:
+        for config in configs:
             print(f'{tp_size=} {bs=}')
+            print(f'{config}')
             # warmup
             print(f'warming up')
             for _ in range(num_warmup_trials):
@@ -41,6 +61,7 @@ def run_grid(method):
                     tp_size=tp_size,
                     model_intermediate_size=model_intermediate_size,
                     method=method,
+                    config=config,
                 )
 
             # trial
@@ -67,7 +88,7 @@ def run_grid(method):
 
 def run_timing(num_calls: int, bs: int, d_model: int, num_total_experts: int,
                top_k: int, tp_size: int, model_intermediate_size: int,
-               method) -> float:
+               method, config) -> float:
     shard_intermediate_size = model_intermediate_size // tp_size
 
     hidden_states = torch.rand(
@@ -110,6 +131,7 @@ def run_timing(num_calls: int, bs: int, d_model: int, num_total_experts: int,
             topk=2,
             renormalize=True,
             inplace=True,
+            config=config,
         )
     end_event.record()
     end_event.synchronize()

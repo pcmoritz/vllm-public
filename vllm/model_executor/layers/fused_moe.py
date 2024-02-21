021eb60c1aa116838bac1de745340eb431328f71
Philipp Moritz <pcmoritz@gmail.com>
2024-02-20 16:03:25 -0800

    update

--- vllm/model_executor/layers/fused_moe.py
+++ vllm/model_executor/layers/fused_moe.py
@@ -83,7 +83,7 @@ def fused_moe_kernel(
     offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
     offs_k = tl.arange(0, BLOCK_SIZE_K)
     a_ptrs = a_ptr + (offs_token[:, None] // top_k * stride_am +
-                      offs_k[None, :] * stride_ak)
+                      offs_k[:, None] * stride_ak)
 
     off_experts = tl.load(expert_ids_ptr + pid_m)
     b_ptrs = b_ptr + off_experts * stride_be + (offs_k[:, None] * stride_bk +
@@ -100,13 +100,13 @@ def fused_moe_kernel(
         # Load the next block of A and B, generate a mask by checking the K dimension.
         a = tl.load(a_ptrs,
                     mask=token_mask[:, None] &
-                    (offs_k[None, :] < K - k * BLOCK_SIZE_K),
+                    (offs_k[:, None] < K - k * BLOCK_SIZE_K),
                     other=0.0)
         b = tl.load(b_ptrs,
                     mask=offs_k[:, None] < K - k * BLOCK_SIZE_K,
                     other=0.0)
         # We accumulate along the K dimension.
-        accumulator += tl.dot(a, b)
+        accumulator += tl.sum(b * a, axis=0)
         # Advance the ptrs to the next K block.
         a_ptrs += BLOCK_SIZE_K * stride_ak
         b_ptrs += BLOCK_SIZE_K * stride_bk

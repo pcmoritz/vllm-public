e0b1987ceec4b208fc2f3bafb971eb5a079329af
Philipp Moritz <pcmoritz@gmail.com>
2024-03-14 20:43:59 -0700

    Implement fused_moe with CUTLASS

--- csrc/moe/moe_ops.h
+++ csrc/moe/moe_ops.h
@@ -7,3 +7,26 @@ void topk_softmax(
   torch::Tensor& topk_indices,
   torch::Tensor& token_expert_indices,
   torch::Tensor& gating_output);
+
+void expand_and_permute(
+  torch::Tensor& permuted_tokens,
+  torch::Tensor& cum_num_tokens_per_expert,
+  torch::Tensor& reverse_permutation_map,
+  torch::Tensor& input_tokens,
+  torch::Tensor& topk_indices,
+  torch::Tensor& token_expert_indices);
+
+void moe_mlp(
+  torch::Tensor& moe_output,
+  torch::Tensor& input_tokens,
+  torch::Tensor& cum_num_tokens_per_expert,
+  torch::Tensor& fc1_expert_weights,
+  torch::Tensor& fc2_expert_weights);
+
+void unpermute_and_reduce(
+  torch::Tensor& output_tokens,
+  torch::Tensor& experts_output,
+  torch::Tensor& topk_weights,
+  torch::Tensor& topk_indices,
+  torch::Tensor& reverse_permutation_map,
+  bool renormalize);

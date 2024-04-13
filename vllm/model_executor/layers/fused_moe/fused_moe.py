0b64025053005517b49d63708904cd485d2aae94
Philipp Moritz <pcmoritz@gmail.com>
2024-04-12 17:14:23 -0700

    kind of working

--- vllm/model_executor/layers/fused_moe/fused_moe.py
+++ vllm/model_executor/layers/fused_moe/fused_moe.py
@@ -145,7 +145,7 @@ def fused_moe_kernel(
                              other=0)
         accumulator = accumulator * moe_weight[:, None]
 
-    accumulator = accumulator.to(compute_type) * w_scale * a_scale
+    accumulator = (accumulator * w_scale * a_scale).to(compute_type)
     # -----------------------------------------------------------
     # Write back the block of the output
     offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
@@ -409,10 +409,20 @@ def fused_moe(
     sorted_token_ids, expert_ids, num_tokens_post_padded = moe_align_block_size(
         topk_ids, config['BLOCK_SIZE_M'], E)
 
-    a_s = a_scale.max()
-    a2_s = a2_scale.max()
+    # print("w_scale", w_scale)
+    # print("w2_scale", w2_scale)
+    # print("a_scale", a_scale)
+    # print("a2_scale", a2_scale)
+
+    a_s = 2.0 * a_scale.max()
+    # a_s = a_scale.max()
+    a2_s = 2.0 * a2_scale.max()
+    # a2_s = a2_scale.max()
 
     ops.scaled_fp8_quant(intermediate_cache0, hidden_states, a_s)
+
+    # print("hidden_states", hidden_states.abs().max())
+    # print("intermediate_cache0", intermediate_cache0.to(torch.float16).abs().max(), "w1", w1.to(torch.float16).abs().max())
 
     invoke_fused_moe_kernel(intermediate_cache0, w1, intermediate_cache1, w_scale, a_s,
                             topk_weights, topk_ids, sorted_token_ids,
@@ -420,6 +430,9 @@ def fused_moe(
                             topk_ids.shape[1], config, compute_type=tl.float16)
 
     ops.scaled_silu_and_mul(intermediate_cache2, intermediate_cache1.view(-1, N), a2_s)
+
+    # print("intermediate_cache1", intermediate_cache1.abs().max())
+    # print("intermediate_cache2", intermediate_cache2.to(torch.float16).abs().max(), "w2", w2.to(torch.float16).abs().max())
 
     invoke_fused_moe_kernel(intermediate_cache2, w2, intermediate_cache3, w2_scale, a2_s,
                             topk_weights, topk_ids, sorted_token_ids,

101c5446be489b9a04d4197bb3e1f5a73bb533a2
Philipp Moritz <pcmoritz@gmail.com>
2024-04-26 14:41:55 -0700

    Add fast dynamic scaling kernel for FP8

--- csrc/quantization/fp8/fp8_cuda_kernels.cu
+++ csrc/quantization/fp8/fp8_cuda_kernels.cu
@@ -72,6 +72,55 @@ __global__ void scaled_fp8_quant_kernel(
   }
 }
 
+// Make it so for batch size 12 and Mixtral-8x22B TP2 the fastpath can still be used
+constexpr int64_t MAX_ACTIVATION_SIZE_FOR_FASTPATH = 12 * 8192;
+
+// The case where the activations fit into shared memory
+template<typename scalar_t>
+__global__ void fast_scaled_fp8_quant_kernel(
+  c10::Float8_e4m3fn* __restrict__ out,
+  const scalar_t* __restrict__ input,
+  const float* __restrict__ scale,
+  int64_t num_elems,
+) {
+  __shared__ float cache[1024];
+  __shared__ scalar_t activations[MAX_ACTIVATION_SIZE_FOR_FASTPATH];
+  int i = threadIdx.x;
+
+  // First store maximum for all values processes by
+  // the current thread in cache[threadIdx.x]
+  scalar_t tmp = 0.0;
+  while (i < num_elems) {
+    activations[i] = input[i];
+    float x = static_cast<float>(activations[i]);
+    tmp = max(tmp, fabs(x));
+    i += blockDim.x * gridDim.x;
+  }
+  cache[threadIdx.x] = tmp;
+
+  __syncthreads();
+
+  // Now perform parallel reduction within the thread block
+  int ib = blockDim.x / 2;
+  while (ib != 0) {
+    if (threadIdx.x < ib && cache[threadIdx.x + ib] > cache[threadIdx.x]) {
+        cache[threadIdx.x] = cache[threadIdx.x + ib];
+    }
+    __syncthreads();
+    ib /= 2;
+  }
+
+  // Now the maximum is in cache[0], rescale and write the results
+  // back to global memory
+  *scale = cache[0];
+
+  i = threadIdx.x;
+  while (i < num_elems) {
+    out[i] = static_cast<c10::Float8_e4m3fn>(input[i] / cache[0]);
+    i += blockDim.x * gridDim.x;
+  }
+}
+
 } // namespace vllm
 
 void scaled_fp8_quant(
@@ -81,23 +130,38 @@ void scaled_fp8_quant(
 {
   int64_t num_tokens = input.numel() / input.size(-1);
   int64_t num_elems = input.numel();
-  dim3 grid(num_tokens);
-  dim3 block(1024);
   const at::cuda::OptionalCUDAGuard device_guard(device_of(input));
   const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-  VLLM_DISPATCH_FLOATING_TYPES(
-    input.scalar_type(),
-    "scaled_fp8_quant_kernel",
-    [&] {
-      vllm::segmented_max_reduction<scalar_t><<<grid, block, 0, stream>>>(
-        scale.data_ptr<float>(),
-        input.data_ptr<scalar_t>(),
-        num_elems);
-      vllm::scaled_fp8_quant_kernel<scalar_t><<<grid, block, 0, stream>>>(
-        out.data_ptr<c10::Float8_e4m3fn>(),
-        input.data_ptr<scalar_t>(),
-        scale.data_ptr<float>(),
-        num_elems);
+  if (num_elems <= MAX_ACTIVATION_SIZE_FOR_FASTPATH) {
+    dim3 grid(1);
+    dim3 block(1024);
+    VLLM_DISPATCH_FLOATING_TYPES(
+      input.scalar_type(),
+      "fast_scaled_fp8_quant_kernel",
+      [&] {
+        vllm::fast_scaled_fp8_quant_kernel<scalar_t><<<grid, block, 0, stream>>>(
+          out.data_ptr<c10::Float8_e4m3fn>(),
+          input.data_ptr<scalar_t>(),
+          scale.data_ptr<float>(),
+          num_elems);
       });
+  } else {
+    dim3 grid(num_tokens);
+    dim3 block(1024);
+    VLLM_DISPATCH_FLOATING_TYPES(
+      input.scalar_type(),
+      "scaled_fp8_quant_kernel",
+      [&] {
+        vllm::segmented_max_reduction<scalar_t><<<grid, block, 0, stream>>>(
+          scale.data_ptr<float>(),
+          input.data_ptr<scalar_t>(),
+          num_elems);
+        vllm::scaled_fp8_quant_kernel<scalar_t><<<grid, block, 0, stream>>>(
+          out.data_ptr<c10::Float8_e4m3fn>(),
+          input.data_ptr<scalar_t>(),
+          scale.data_ptr<float>(),
+          num_elems);
+        });
+  }
 }
 

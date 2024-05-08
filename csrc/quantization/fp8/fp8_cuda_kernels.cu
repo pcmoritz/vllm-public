72c90c1680a090508bb5c349b248cb8c23d26250
Philipp Moritz <pcmoritz@gmail.com>
2024-05-07 17:20:49 -0700

    Add initial code

--- csrc/quantization/fp8/fp8_cuda_kernels.cu
+++ csrc/quantization/fp8/fp8_cuda_kernels.cu
@@ -133,3 +133,89 @@ void dynamic_scaled_fp8_quant(
       });
 }
 
+void fp8_scaled_gemm(torch::Tensor& out, torch::Tensor& input, torch::Tensor& weights, torch::Tensor& workspace) {
+  cublasLtHandle_t ltHandle = at::cuda::getCurrentCUDABlasLtHandle();
+
+  int m = input.size(0);
+  int n = weights.size(1);
+  int k = weights.size(0);
+
+  int lda = k;
+  int ldb = k;
+  int ldc = m;
+
+  uint8_t* workspace = workspace.data_ptr<uint8_t>();
+  size_t workspaceSize = workspace.numel();
+
+  float alpha = 1.0;
+
+  cublasLtMatmulDesc_t operationDesc = NULL;
+  cublasLtMatrixLayout_t Adesc = NULL, Bdesc = NULL, Cdesc = NULL, Ddesc = NULL;
+  cublasLtMatmulPreference_t preference = NULL;
+
+  cublasOperation_t transa = CUBLAS_OP_T;
+  cublasOperation_t transb = CUBLAS_OP_N;
+  float beta = 0.0;
+
+  int returnedResults                             = 0;
+  cublasLtMatmulHeuristicResult_t heuristicResult = {};
+
+  // create operation desciriptor; see cublasLtMatmulDescAttributes_t for details about defaults; here we just need to
+  // set the transforms for A and B
+  checkCublasStatus(cublasLtMatmulDescCreate(&operationDesc, CUBLAS_COMPUTE_32F, CUDA_R_32F));
+  checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_TRANSA, &transa, sizeof(transa)));
+  checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_TRANSB, &transb, sizeof(transb)));
+
+  // set scaling factors
+  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_A_SCALE_POINTER, &a_scale, sizeof(a_scale)));
+  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_B_SCALE_POINTER, &b_scale, sizeof(b_scale)));
+  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_C_SCALE_POINTER, &c_scale, sizeof(c_scale)));
+  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_D_SCALE_POINTER, &d_scale, sizeof(d_scale)));
+  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_AMAX_D_POINTER, &amax_d, sizeof(amax_d)));
+
+  // create matrix descriptors, we are good with the details here so no need to set any extra attributes
+  // table of supported type combinations can be found in the documentation: https://docs.nvidia.com/cuda/cublas/index.html#cublasltmatmul
+  checkCublasStatus(cublasLtMatrixLayoutCreate(&Adesc, CUDA_R_8F_E4M3, transa == CUBLAS_OP_N ? m : k, transa == CUBLAS_OP_N ? k : m, lda));
+  checkCublasStatus(cublasLtMatrixLayoutCreate(&Bdesc, CUDA_R_8F_E4M3, transb == CUBLAS_OP_N ? k : n, transb == CUBLAS_OP_N ? n : k, ldb));
+  checkCublasStatus(cublasLtMatrixLayoutCreate(&Cdesc, CUDA_R_16BF, m, n, ldc));
+  checkCublasStatus(cublasLtMatrixLayoutCreate(&Ddesc, CUDA_R_8F_E4M3, m, n, ldc));
+
+  // create preference handle; here we could use extra attributes to disable tensor ops or to make sure algo selected
+  // will work with badly aligned A, B, C; here for simplicity we just assume A,B,C are always well aligned (e.g.
+  // directly come from cudaMalloc)
+  checkCublasStatus(cublasLtMatmulPreferenceCreate(&preference));
+  checkCublasStatus(cublasLtMatmulPreferenceSetAttribute(preference, CUBLASLT_MATMUL_PREF_MAX_WORKSPACE_BYTES, &workspaceSize, sizeof(workspaceSize)));
+
+  // we just need the best available heuristic to try and run matmul. There is no guarantee this will work, e.g. if A
+  // is badly aligned, you can request more (e.g. 32) algos and try to run them one by one until something works
+  checkCublasStatus(cublasLtMatmulAlgoGetHeuristic(ltHandle, operationDesc, Adesc, Bdesc, Cdesc, Ddesc, preference, 1, &heuristicResult, &returnedResults));
+
+  if (returnedResults == 0) {
+      checkCublasStatus(CUBLAS_STATUS_NOT_SUPPORTED);
+  }
+
+  checkCublasStatus(cublasLtMatmul(ltHandle,
+                                   operationDesc,
+                                   &alpha,
+                                   A,
+                                   Adesc,
+                                   B,
+                                   Bdesc,
+                                   &beta,
+                                   nullptr,
+                                   Cdesc,
+                                   D,
+                                   Ddesc,
+                                   &heuristicResult.algo,
+                                   workspace,
+                                   workspaceSize,
+                                   0));
+
+  // descriptors are no longer needed as all GPU work was already enqueued
+  if (preference) checkCublasStatus(cublasLtMatmulPreferenceDestroy(preference));
+  if (Ddesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Ddesc));
+  if (Cdesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Cdesc));
+  if (Bdesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Bdesc));
+  if (Adesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Adesc));
+  if (operationDesc) checkCublasStatus(cublasLtMatmulDescDestroy(operationDesc));
+}

4954cdaca8583f87551cdf29b1076f9067e53ad0
Philipp Moritz <pcmoritz@gmail.com>
2024-05-07 17:31:35 -0700

    update

--- csrc/quantization/fp8/fp8_cuda_kernels.cu
+++ csrc/quantization/fp8/fp8_cuda_kernels.cu
@@ -167,39 +167,39 @@ void fp8_scaled_gemm(torch::Tensor& out, torch::Tensor& input, torch::Tensor& we
 
   // create operation desciriptor; see cublasLtMatmulDescAttributes_t for details about defaults; here we just need to
   // set the transforms for A and B
-  checkCublasStatus(cublasLtMatmulDescCreate(&operationDesc, CUBLAS_COMPUTE_32F, CUDA_R_32F));
-  checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_TRANSA, &transa, sizeof(transa)));
-  checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_TRANSB, &transb, sizeof(transb)));
+  TORCH_CUDABLAS_CHECK(cublasLtMatmulDescCreate(&operationDesc, CUBLAS_COMPUTE_32F, CUDA_R_32F));
+  TORCH_CUDABLAS_CHECK(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_TRANSA, &transa, sizeof(transa)));
+  TORCH_CUDABLAS_CHECK(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_TRANSB, &transb, sizeof(transb)));
 
   // set scaling factors
-  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_A_SCALE_POINTER, &a_scale, sizeof(a_scale)));
-  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_B_SCALE_POINTER, &b_scale, sizeof(b_scale)));
-  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_C_SCALE_POINTER, &c_scale, sizeof(c_scale)));
-  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_D_SCALE_POINTER, &d_scale, sizeof(d_scale)));
-  // checkCublasStatus(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_AMAX_D_POINTER, &amax_d, sizeof(amax_d)));
+  // TORCH_CUDABLAS_CHECK(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_A_SCALE_POINTER, &a_scale, sizeof(a_scale)));
+  // TORCH_CUDABLAS_CHECK(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_B_SCALE_POINTER, &b_scale, sizeof(b_scale)));
+  // TORCH_CUDABLAS_CHECK(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_C_SCALE_POINTER, &c_scale, sizeof(c_scale)));
+  // TORCH_CUDABLAS_CHECK(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_D_SCALE_POINTER, &d_scale, sizeof(d_scale)));
+  // TORCH_CUDABLAS_CHECK(cublasLtMatmulDescSetAttribute(operationDesc, CUBLASLT_MATMUL_DESC_AMAX_D_POINTER, &amax_d, sizeof(amax_d)));
 
   // create matrix descriptors, we are good with the details here so no need to set any extra attributes
   // table of supported type combinations can be found in the documentation: https://docs.nvidia.com/cuda/cublas/index.html#cublasltmatmul
-  checkCublasStatus(cublasLtMatrixLayoutCreate(&Adesc, CUDA_R_8F_E4M3, transa == CUBLAS_OP_N ? m : k, transa == CUBLAS_OP_N ? k : m, lda));
-  checkCublasStatus(cublasLtMatrixLayoutCreate(&Bdesc, CUDA_R_8F_E4M3, transb == CUBLAS_OP_N ? k : n, transb == CUBLAS_OP_N ? n : k, ldb));
-  checkCublasStatus(cublasLtMatrixLayoutCreate(&Cdesc, CUDA_R_16BF, m, n, ldc));
-  checkCublasStatus(cublasLtMatrixLayoutCreate(&Ddesc, CUDA_R_8F_E4M3, m, n, ldc));
+  TORCH_CUDABLAS_CHECK(cublasLtMatrixLayoutCreate(&Adesc, CUDA_R_8F_E4M3, transa == CUBLAS_OP_N ? m : k, transa == CUBLAS_OP_N ? k : m, lda));
+  TORCH_CUDABLAS_CHECK(cublasLtMatrixLayoutCreate(&Bdesc, CUDA_R_8F_E4M3, transb == CUBLAS_OP_N ? k : n, transb == CUBLAS_OP_N ? n : k, ldb));
+  TORCH_CUDABLAS_CHECK(cublasLtMatrixLayoutCreate(&Cdesc, CUDA_R_16BF, m, n, ldc));
+  TORCH_CUDABLAS_CHECK(cublasLtMatrixLayoutCreate(&Ddesc, CUDA_R_8F_E4M3, m, n, ldc));
 
   // create preference handle; here we could use extra attributes to disable tensor ops or to make sure algo selected
   // will work with badly aligned A, B, C; here for simplicity we just assume A,B,C are always well aligned (e.g.
   // directly come from cudaMalloc)
-  checkCublasStatus(cublasLtMatmulPreferenceCreate(&preference));
-  checkCublasStatus(cublasLtMatmulPreferenceSetAttribute(preference, CUBLASLT_MATMUL_PREF_MAX_WORKSPACE_BYTES, &workspaceSize, sizeof(workspaceSize)));
+  TORCH_CUDABLAS_CHECK(cublasLtMatmulPreferenceCreate(&preference));
+  TORCH_CUDABLAS_CHECK(cublasLtMatmulPreferenceSetAttribute(preference, CUBLASLT_MATMUL_PREF_MAX_WORKSPACE_BYTES, &workspaceSize, sizeof(workspaceSize)));
 
   // we just need the best available heuristic to try and run matmul. There is no guarantee this will work, e.g. if A
   // is badly aligned, you can request more (e.g. 32) algos and try to run them one by one until something works
-  checkCublasStatus(cublasLtMatmulAlgoGetHeuristic(ltHandle, operationDesc, Adesc, Bdesc, Cdesc, Ddesc, preference, 1, &heuristicResult, &returnedResults));
+  TORCH_CUDABLAS_CHECK(cublasLtMatmulAlgoGetHeuristic(ltHandle, operationDesc, Adesc, Bdesc, Cdesc, Ddesc, preference, 1, &heuristicResult, &returnedResults));
 
   if (returnedResults == 0) {
-      checkCublasStatus(CUBLAS_STATUS_NOT_SUPPORTED);
+      TORCH_CUDABLAS_CHECK(CUBLAS_STATUS_NOT_SUPPORTED);
   }
 
-  checkCublasStatus(cublasLtMatmul(ltHandle,
+  TORCH_CUDABLAS_CHECK(cublasLtMatmul(ltHandle,
                                    operationDesc,
                                    &alpha,
                                    A,
@@ -217,10 +217,10 @@ void fp8_scaled_gemm(torch::Tensor& out, torch::Tensor& input, torch::Tensor& we
                                    0));
 
   // descriptors are no longer needed as all GPU work was already enqueued
-  if (preference) checkCublasStatus(cublasLtMatmulPreferenceDestroy(preference));
-  if (Ddesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Ddesc));
-  if (Cdesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Cdesc));
-  if (Bdesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Bdesc));
-  if (Adesc) checkCublasStatus(cublasLtMatrixLayoutDestroy(Adesc));
-  if (operationDesc) checkCublasStatus(cublasLtMatmulDescDestroy(operationDesc));
+  if (preference) TORCH_CUDABLAS_CHECK(cublasLtMatmulPreferenceDestroy(preference));
+  if (Ddesc) TORCH_CUDABLAS_CHECK(cublasLtMatrixLayoutDestroy(Ddesc));
+  if (Cdesc) TORCH_CUDABLAS_CHECK(cublasLtMatrixLayoutDestroy(Cdesc));
+  if (Bdesc) TORCH_CUDABLAS_CHECK(cublasLtMatrixLayoutDestroy(Bdesc));
+  if (Adesc) TORCH_CUDABLAS_CHECK(cublasLtMatrixLayoutDestroy(Adesc));
+  if (operationDesc) TORCH_CUDABLAS_CHECK(cublasLtMatmulDescDestroy(operationDesc));
 }

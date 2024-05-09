bbc1c2e14993f8ed49aa8501edadb4b1b1935a19
Philipp Moritz <pcmoritz@gmail.com>
2024-05-08 21:12:36 -0700

    update

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -686,7 +686,7 @@ class RowParallelLinear(LinearBase):
 
         # Matrix multiply.
         assert self.quant_method is not None
-        output_parallel = self.quant_method.apply(self, input_parallel, out_dtype)
+        output_parallel, output_scale = self.quant_method.apply(self, input_parallel, out_dtype)
         if self.reduce_results and self.tp_size > 1:
             output_ = tensor_model_parallel_all_reduce(output_parallel)
         else:
@@ -698,7 +698,7 @@ class RowParallelLinear(LinearBase):
         else:
             output = output_
             output_bias = self.bias
-        return output, output_bias
+        return output, output_bias, output_scale
 
     def extra_repr(self) -> str:
         s = f"input_features={self.input_size_per_partition}"

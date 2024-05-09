dfb8b3b8246f38a6baf48e9ee5c1cbb794d73814
Philipp Moritz <pcmoritz@gmail.com>
2024-05-08 20:52:58 -0700

    fix

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -161,12 +161,12 @@ class LlamaAttention(nn.Module):
         kv_cache: torch.Tensor,
         attn_metadata: AttentionMetadata,
     ) -> torch.Tensor:
-        qkv, _ = self.qkv_proj(hidden_states)
+        qkv, _, _ = self.qkv_proj(hidden_states)
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
         q, k = self.rotary_emb(positions, q, k)
         attn_output = self.attn(q, k, v, kv_cache, attn_metadata,
                                 self.kv_scale)
-        output, _ = self.o_proj(attn_output)
+        output, _, _ = self.o_proj(attn_output)
         return output
 
 

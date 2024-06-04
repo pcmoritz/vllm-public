4b5aaed362b48896e82f295f44bcbb1438219a73
Philipp Moritz <pcmoritz@gmail.com>
2024-06-04 16:58:37 -0700

    Add fused_silu

---
+++ vllm/model_executor/layers/fused_silu/fused_silu.py
@@ -0,0 +1,148 @@
+import functools
+import json
+import os
+from typing import Optional, Any, Dict
+
+import torch
+
+import triton
+import triton.language as tl
+
+from vllm.logger import init_logger
+
+logger = init_logger(__name__)
+
+@triton.jit
+def silu(input):
+    return input / (1 + tl.exp(-input))
+
+
+@triton.jit
+def matmul_kernel(
+        # Pointers to matrices
+        a_ptr, b1_ptr, b2_ptr, c_ptr,
+        a_scale_ptr, b_scale_ptr, c_scale_ptr,
+        # Matrix dimensions
+        M, N, K,
+        # The stride variables represent how much to increase the ptr by when moving by 1
+        # element in a particular dimension. E.g. `stride_am` is how much to increase `a_ptr`
+        # by to get the element one row down (A has M rows).
+        stride_am, stride_ak,  #
+        stride_bk, stride_bn,  #
+        stride_cm, stride_cn,
+        # Meta-parameters
+        BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,  #
+        GROUP_SIZE_M: tl.constexpr,  #
+):
+    """Kernel for computing the matmul C = A x B.
+    A has shape (M, K), B has shape (K, N) and C has shape (M, N)
+    """
+    # -----------------------------------------------------------
+    # Map program ids `pid` to the block of C it should compute.
+    # This is done in a grouped ordering to promote L2 data reuse.
+    # See above `L2 Cache Optimizations` section for details.
+    pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+    num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+    num_pid_in_group = GROUP_SIZE_M * num_pid_n
+    group_id = pid // num_pid_in_group
+    first_pid_m = group_id * GROUP_SIZE_M
+    group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+    pid_m = first_pid_m + (pid % group_size_m)
+    pid_n = (pid % num_pid_in_group) // group_size_m
+
+    a_scale = tl.load(a_scale_ptr)
+    b_scale = tl.load(b_scale_ptr)
+    ab_scale = a_scale * b_scale
+    c_scale = tl.load(c_scale_ptr)
+
+    # ----------------------------------------------------------
+    # Create pointers for the first blocks of A and B.
+    # We will advance this pointer as we move in the K direction
+    # and accumulate
+    # `a_ptrs` is a block of [BLOCK_SIZE_M, BLOCK_SIZE_K] pointers
+    # `b_ptrs` is a block of [BLOCK_SIZE_K, BLOCK_SIZE_N] pointers
+    # See above `Pointer Arithmetic` section for details
+    offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+    offs_k = tl.arange(0, BLOCK_SIZE_K)
+    a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
+    b1_ptrs = b1_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+    b2_ptrs = b2_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+
+    # -----------------------------------------------------------
+    # Iterate to compute a block of the C matrix.
+    # We accumulate into a `[BLOCK_SIZE_M, BLOCK_SIZE_N]` block
+    # of fp32 values for higher accuracy.
+    # `accumulator` will be converted back to fp16 after the loop.
+    acc1 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    acc2 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
+        # Load the next block of A and B, generate a mask by checking the K dimension.
+        # If it is out of bounds, set it to 0.
+        a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)
+        b1 = tl.load(b1_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
+        b2 = tl.load(b2_ptrs, mask=offs_k[:, None] < K - k * BLOCK_SIZE_K, other=0.0)
+        # We accumulate along the K dimension.
+        acc1 = tl.dot(a, b1, acc1)
+        acc2 = tl.dot(a, b2, acc2)
+        # Advance the ptrs to the next K block.
+        a_ptrs += BLOCK_SIZE_K * stride_ak
+        b1_ptrs += BLOCK_SIZE_K * stride_bk
+        b2_ptrs += BLOCK_SIZE_K * stride_bk
+
+    c = (silu(acc1 * ab_scale) * acc2 * ab_scale / c_scale).to(tl.float8e4nv)
+
+    # -----------------------------------------------------------
+    # Write back the block of the output matrix C with masks.
+    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
+    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+    tl.store(c_ptrs, c, mask=c_mask)
+
+@functools.lru_cache
+def get_silu_configs() -> Optional[Dict[int, Any]]:
+    config_file_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "config.json")
+    if os.path.exists(config_file_path):
+        with open(config_file_path) as f:
+            logger.info("Using configuration from %s for silu layer.",
+                        config_file_path)
+            # If a configuration has been found, return it
+            return {int(key): val for key, val in json.load(f).items()}
+
+    # If no optimized configuration is available, we will use the default
+    # configuration
+    return None
+
+def fused_silu(a, b1, b2, a_scale, b_scale, c_scale, override_config: Optional[Dict[str, Any]] = None):
+
+    if override_config:
+        config = override_config
+    else:
+        configs = get_silu_configs()
+        assert configs
+        config = configs[min(configs.keys(), key=lambda x: abs(x - M))]
+
+    # Check constraints.
+    assert a.shape[1] == b1.shape[0], f"Incompatible dimensions {a.shape[1]} != {b1.shape[0]}"
+    assert a.shape[1] == b2.shape[0], f"Incompatible dimensions {a.shape[1]} != {b2.shape[0]}"
+    assert b1.shape[1] == b2.shape[1], f"Incompatble dimensions {b1.shape[1]} != {b2.shape[1]}"
+    assert a.is_contiguous(), "Matrix A must be contiguous"
+    M, K = a.shape
+    K, N = b1.shape
+    # Allocates output.
+    c = torch.empty((M, N), device=a.device, dtype=torch.float8_e4m3fn)
+    # 1D launch kernel where each block gets its own program.
+    grid = lambda META: (triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']), )
+    matmul_kernel[grid](
+        a, b1, b2, c,
+        a_scale, b_scale, c_scale,
+        M, N, K,
+        a.stride(0), a.stride(1),
+        b1.stride(0), b1.stride(1),
+        c.stride(0), c.stride(1),
+        **config,
+    )
+    return c
